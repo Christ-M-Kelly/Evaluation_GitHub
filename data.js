129a6c2e15cc215dac240c57a165b95091893652
Keliane Kossa <keliane-serane-moaye.kossa@supdevinci-edu.fr>
2025-10-16 15:30:07 +0200

    Retour à la modif 3

--- data.js
+++ data.js
@@ -4,32 +4,20 @@ const books = [
     author: "Antoine de Saint-Exupéry",
     Date: "1940",
     Genre: "Roman",
-<<<<<<< HEAD
-    lang : "Fr",
-=======
     language : "Français",
->>>>>>> parent of dc6ac79 ( Troisième modification)
   },
   {
     title: "1984",
     author: "George Orwell",
     Date: "1949",
     Genre: "Dystopie",
-<<<<<<< HEAD
-    lang : "En",
-=======
     language : "Anglais",
->>>>>>> parent of dc6ac79 ( Troisième modification)
   },
   {
     title: "Les Misérables",
     author: "Victor Hugo",
     Date: "1862",
     Genre: "Roman historique",
-<<<<<<< HEAD
-    lang : "Fr",
-=======
     language : "Français",
->>>>>>> parent of dc6ac79 ( Troisième modification)
   },
 ];
